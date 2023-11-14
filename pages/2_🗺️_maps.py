ed3545b0429ecae0584773e858adb038372ba94b
Humberto Cordioli <hcordioli@gmail.com>
2023-11-14 13:19:51 -0300

    Update 2_🗺️_maps.py

--- pages/2_🗺️_maps.py
+++ pages/2_🗺️_maps.py
@@ -5,7 +5,6 @@ import numpy as np
 import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
