37a60328b2c11358320c761b95e904d7a895f0f0
Humberto Cordioli <hcordioli@gmail.com>
2023-11-23 10:31:27 -0300

    Add files via upload
    Inclusão das OBSERVAÇÔES IMPORTANTES, sobre a restrição de dados

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 """ ITBI Home Page 
 
-Página inicial do Dashboard do dataset ITBI
+Página inicial do Dashboard do dataset ITBI.
 
 ITBI é o Imposto sobre Transferência de Bens Imóveis
 A Prefeitura de São Paulo disponibiliza em planilhas, o registro de cada transação em 
@@ -27,5 +27,16 @@ st.markdown(
         https://www.prefeitura.sp.gov.br/cidade/secretarias/fazenda/acesso_a_informacao/index.php?p=31501
     
     Este Dashboard pretende elucidar estes dados.
+    
+    OBSERVAÇÃO IMPORTANTE:
+    
+    * Idealmente, um período contínuo de 3 anos seria necessário para este trabalho.
+    Com isso, poderíamos incluir até uma previsão de arrecadação usando série temporal (ARIMA).
+    Infelizmente, como podem notar no site da Prefeitura de São Paulo, os arquivos entre os anos de 
+    2019 a 2022 ainda não estão disponíveis.
+    O impacto neste trabalho é:
+        * "Buracos" ou falta de dados durante estes anos, em alguns charts 
+        * Análises restritas ao ano de 2023, em alguns charts
+        * Falta da previsão de recadação do imposto ITBI
 """
 )
